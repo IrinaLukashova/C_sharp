436228ed4ed06ab0131c5fda2ec05072f9068bac
irina <irisha.201@mail.com>
2023-08-07 00:58:25 +0300

    009

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -4,3 +4,8 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine(number);
+
+int first = number / 10;
+int second = number % 10;
+if (first > second) Console.WriteLine($"Наибольшая цифра числа {number} это {first}");
+else Console.WriteLine($"Наибольшая цифра числа {number} это {second}");

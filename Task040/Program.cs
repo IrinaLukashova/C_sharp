1fc243514708983d810fe0b28731ccf9b345c04d
irina <irisha.201@mail.com>
2023-09-02 00:38:26 +0300

    6 семинар, задачи 40,42,44,45

---
+++ Task040/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+40. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+*/
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.XPath;
+
+Console.Write("Введите длины сторон: ");
+int[] FillUserArray()
+{
+    int[] array = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+bool TriangleExistance(int[] array)
+{
+    if (array[0] < array[1] + array[2]
+        && array[1] < array[0] + array[2]
+        && array[2] < array[1] + array[0])
+    {    
+        return true;
+    }
+    else return false;
+}
+
+bool result = TriangleExistance(FillUserArray());
+Console.WriteLine(result);
---
+++ Task045/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+45. Напишите программу, которая будет создавать копию заданного одномерного массива 
+с помощью поэлементного копирования.
+*/
+
+int[] array = {1, 2, 3, 4, 8};
+
+int[] ArrayCopy(int[] array)
+{
+    int[] newarray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newarray[i] = array[i];
+    }
+    return newarray;
+}
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.WriteLine("]");
+}
+PrintArray(array);
+PrintArray(ArrayCopy(array));

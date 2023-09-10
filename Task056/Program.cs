0b998ed05cd0b61448a614560b8b3648e62ec7dc
irina <irisha.201@mail.com>
2023-09-10 18:41:43 +0300

    Дз по 8 семинару 54,56,58,60,62

---
+++ Task056/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+наименьшей суммой элементов: 1 строка
+ */
+
+ 
+int[,] GetRandom2DArray(int rowNumber, int min, int max)
+{
+    int[,] result = new int[rowNumber, rowNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < rowNumber; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}] \t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}] \t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumRows(int[,] array)
+{
+    int[] sumarray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sumarray[i] = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumarray[i] += array[i,j];
+        }
+    }
+    return sumarray;
+}
+
+int Min(int[] array)
+{
+    int minrow = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < array[minrow]) minrow = i;
+    }
+    return minrow;
+}
+
+int[,] userArray = GetRandom2DArray(5,0,10);
+Print2DArray(userArray);
+System.Console.WriteLine($"Строка с наименьшей суммой элементов - {Min(SumRows(userArray))}");
---
+++ Task062/Program.cs
@@ -0,0 +1,83 @@
+﻿/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07 
+   for (int i = 0; i < result.GetLength(1); i++)
+    {
+        result[0,i] = count;
+        count++;
+    }
+    for (int i = 1; i < result.GetLength(0); i++)
+    {
+        result[i,result.GetLength(1)-1] = count;
+        count++;
+    }
+    for (int i = result.GetLength(1)-2; i >= 0; i = i-1)
+    {
+        result[result.GetLength(0)-1,i] = count;
+        count++;
+    }
+    for (int i = result.GetLength(0)-2; i >0; i = i-1)
+    {
+       result[i,0] = count;
+        count++; 
+    }  
+    return result;*/
+
+int[,] GetSpiral(int size)
+{
+    int[,] result = new int[size,size];
+    int count = 1;
+    int var = size;
+    int k = (var+1)/2;
+    while (k > 0)
+    {
+        for (int i = size - var; i < var; i++)
+        {
+            result[size - var,i] = count;
+            count++;
+        }
+        for (int j = size - var + 1; j < var; j++)
+        {
+            result[j,var -1] = count;
+            count++;
+        }
+        for (int i = var - 2; i >= size - var; i= i -1)
+        {
+            result[var -1,i] = count;
+            count++;
+        }
+        for (int j = var - 2; j >= size - var +1; j = j-1)
+        {
+            result[j,size-var] = count;
+            count++;
+        }
+        k -= 1;
+        var -= 1;
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}] \t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}] \t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] userArray = GetSpiral(5); // сделала для любого размера спирали
+Print2DArray(userArray);
